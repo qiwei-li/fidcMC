40a6484bb5aedc9ca6796f3bdb9feb85bc79a3ce
Qiwei <liqiwei1224@gmail.com>
2016-01-19 21:59:02 -0800

    object parser done. Need to work on core functions

---
+++ R/create.R
@@ -0,0 +1,40 @@
+mc.create = function(pijdef, qidef=NULL, discrete, infinite){
+  if(discrete==TRUE & infinite==FALSE){
+    if(class(pijdef)!="matrix")
+      stop("ERROR: pijdef needs to be a matrix")
+    if(nrow(pijdef) != ncol(pijdef))
+      stop("ERROR: pijdef needs to be a square matrix")
+    mc = list(pijdef=pijdef, qidef=NULL, type = "DF")
+    return(mc)
+  }
+  
+  if(discrete==TRUE & infinite==TRUE){
+    if(class(pijdef)!="function")
+      stop("ERROR: pijdef needs to be a function with input: (i,j) and output: a probability from i to j")
+    mc = list(pijdef=pijdef, qidef=NULL, type = "DI")
+    return(mc)
+  }
+  
+  if(discrete==FALSE & infinite==FALSE){
+    if(class(pijdef)!="matrix")
+      stop("ERROR: pijdef needs to be a matrix")
+    if(class(qidef)!="numeric" & class(qidef)!="integer")
+      stop("ERROR: qidef needs to be a numerical vector")
+    if(nrow(pijdef) != ncol(pijdef))
+      stop("ERROR: pijdef needs to be a square matrix")
+    if(nrow(pijdef) != length(qidef))
+      stop("ERROR: numbers of stages from pijdef and qijdef need to agree")
+    mc = list(pijdef=pijdef, qidef=qidef, type = "CF")
+    return(mc)
+  }
+
+  
+  if(discrete==FALSE & infinite==TRUE){
+    if(class(pijdef)!="function")
+      stop("ERROR: pijdef needs to be a function with input: (i,j) and output: a probability from i to j")
+    if(class(qidef)!="function")
+      stop("ERROR: qidef needs to be a function which input: i and output: the lamda of the holding time")
+    mc = list(pijdef=pijdef, qidef=qidef, type = "CI")
+    return(mc)
+  }
+}
